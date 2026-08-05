762aec24db810120853ad6fe6f75321266f67033
dhanushkaj <dm.jaya63@gmail.com>
2026-08-05 08:12:35 +0530

    10043F_Adding_export_capability_to_physica_count_screen

--- src/pages/InventoryCountManagement/HistoryViewer.js
+++ src/pages/InventoryCountManagement/HistoryViewer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import * as XLSX from 'xlsx';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../components/AuthContext';
 import '../../css/InventoryCount.css';
